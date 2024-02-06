e0e37dbfc983da0a32f27975ba9254b3f30311d7
Mateusz-Musik <mateusz.musik1@gmail.com>
2024-02-06 21:14:37 +0100

    added changing colour function

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+//funkcja generuje losowy kolor w formacie szesnastkowym
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+
+//querySelector wyszukuje przyciski START i STOP
+const startBtn = document.querySelector('[data-start'); // błąd!:[start-stop]
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId = 0;
+
+//po naciśnięciu START funkcja zmienia kolor 1 raz na sekundę.
+function startColorSwitching() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        body.style.backgroundColor = randomColor;
+    }, 1000);
+}
+
+//po naciśnięciu STOP zmiana koloru zostaje zatrzymana.
+function stopColorSwitching() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+}
+
+//Eventy(zdarzenia) przypisane do START i STOP
+startBtn.addEventListener('click', startColorSwitching);
+stopBtn.addEventListener('click', stopColorSwitching);
